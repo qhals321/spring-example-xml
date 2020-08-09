83451fcfb1781ca4fab77b494f4e729d02106033
bomin <bomdani9302@gmail.com>
2020-08-09 19:49:39 +0900

    일단 다 커밋 시켜놓음

--- src/main/java/spring_example/main/Main.java
+++ src/main/java/spring_example/main/Main.java
@@ -1,18 +1,33 @@
 package spring_example.main;
 
+import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
 import spring_example.beans.HelloWorld;
 
 public class Main {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("config/beans.xml");
+        //beanFactory();
+        applicationContext();
 
+    }
+
+    public static void callMethod(HelloWorld helloWorld){
+        helloWorld.sayhello();
+    }
+
+    public static void applicationContext(){
+        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("config/beans.xml");
         HelloWorld helloWorld = (HelloWorld) ctx.getBean("hello");
         callMethod(helloWorld);
         ctx.close();
     }
 
-    public static void callMethod(HelloWorld helloWorld){
-        helloWorld.sayhello();
+    public static void beanFactory(){
+        ClassPathResource res = new ClassPathResource("config/beans.xml");
+        XmlBeanFactory factory = new XmlBeanFactory(res);
+
+        HelloWorld helloWorld = (HelloWorld) factory.getBean("hello");
+        callMethod(helloWorld);
     }
 }
